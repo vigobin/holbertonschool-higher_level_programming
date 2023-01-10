81af637f7a42fe4cc03444ca5888797ea185e35f
vigobin <vgobin@outlook.com>
2023-01-10 16:59:15 +0000

    Added period for 0 arguments

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     number = len(sys.argv) - 1
 
     if number == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif number == 1:
         print("1 argument:")
     else:
