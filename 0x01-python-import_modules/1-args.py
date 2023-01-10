1a05dca20d8e0dd98147ca70b9d77eafda7b455a
vigobin <vgobin@outlook.com>
2023-01-10 16:57:22 +0000

    Added file for task 1

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    number = len(sys.argv) - 1
+
+    if number == 0:
+        print("0 arguments")
+    elif number == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(number))
+    for i in range(number):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
