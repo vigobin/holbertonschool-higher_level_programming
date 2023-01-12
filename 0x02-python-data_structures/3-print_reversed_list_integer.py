ba84bc98dc86e199f089d39fd10b6defb7db37e0
vigobin <vgobin@outlook.com>
2023-01-12 21:09:29 +0000

    Updated to use reversed function

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
     if my_list:
-        for i in my_list:
+        for i in reversed(my_list):
             print("{:d}".format(i))
     else:
         return None
