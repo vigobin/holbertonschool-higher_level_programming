e5b1c7910ea5849a635cc46ae194cecd9769b4e7
vigobin <vgobin@outlook.com>
2023-01-16 17:13:50 +0000

    Included tuple length check

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-
-    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    if len(tuple_a) < 2:
+        tuple_a = 0, 0
+    if len(tuple_b) < 2:
+        tuple_b = 0, 0
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
