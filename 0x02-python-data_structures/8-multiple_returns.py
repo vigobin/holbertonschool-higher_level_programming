ba37e910fd06fc977d4f3677d58e3fcd6ab4caf5
vigobin <vgobin@outlook.com>
2023-01-16 15:32:25 +0000

    Initial commit for task 8

---
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        first = None
+    else:
+        first = sentence[0]
+    t = length, first
+    return t
