abd6e5fe25974d4f93161eeb41682ef5171434d6
vigobin <vgobin@outlook.com>
2023-01-16 16:03:31 +0000

    Updated if to use i

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for i in my_list:
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
