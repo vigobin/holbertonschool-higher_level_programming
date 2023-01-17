938f678b592f35d70945a2e376b7d9f0ca682162
vigobin <vgobin@outlook.com>
2023-01-17 15:52:38 +0000

    updated to be three lines

--- 0x03-python-more_data_structures/11-multiply_list_map.py
+++ 0x03-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    mult_list = list(map(lambda x: x * number, my_list))
-    return mult_list
+    return list(map(lambda x: x * number, my_list))
