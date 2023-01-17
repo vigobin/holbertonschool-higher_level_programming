eaab7b6a23b372bb7b352241872b50312b3d1e22
vigobin <vgobin@outlook.com>
2023-01-17 16:27:58 +0000

    included documentation

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""This is the square class"""
+
+
 class Square:
-    pass
+    """This class defines a square by it's private instance attribute size
+    """
+    def __init__(self, size):
+        """Instantation with size
+        Args:
+            size (_no type_): size of square
+        """
+        self.__size = size
