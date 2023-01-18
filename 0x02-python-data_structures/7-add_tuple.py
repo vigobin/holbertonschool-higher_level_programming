389b0dba9342b71c1dd058e307911f94941e1ca3
vigobin <vgobin@outlook.com>
2023-01-18 22:47:07 +0000

    updated type on tuple

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 1:
         tuple_a = (0, 0)
     elif len(tuple_a) == 1:
-        tuple_a = (tupe_a[0], 0)
+        tuple_a = (tuple_a[0], 0)
     if len(tuple_b) < 1:
         tuple_b = (0, 0)
     elif len(tuple_b) == 1:
