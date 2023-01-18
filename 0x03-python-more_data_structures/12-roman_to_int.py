c75412c642aa03c5a28a30778b9cb1aaec93e2ce
vigobin <vgobin@outlook.com>
2023-01-18 22:30:40 +0000

    Added file for task 12

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000}
+    sum = 0
+    length = len(roman_string)
+    for i in range(length):
+        if i + 1 < length and roman_dict[roman_string[i]] <\
+                roman_dict[roman_string[i + 1]]:
+            sum -= roman_dict[roman_string[i]]
+        else:
+            sum += roman_dict[roman_string[i]]
+    return sum
