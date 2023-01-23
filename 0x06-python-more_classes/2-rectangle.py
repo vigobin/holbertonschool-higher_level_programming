de1f00e081a0211491c7894079beb566f7f4f39d
vigobin <vgobin@outlook.com>
2023-01-23 14:53:26 -0600

    Added empty string return

--- 0x06-python-more_classes/2-rectangle.py
+++ 0x06-python-more_classes/2-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
-            return ()
+            return ("")
         print_rectangle = ""
         for i in range(self.__height):
             if i == self.__height - 1:
