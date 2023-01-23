5316356237ec191787cb1206d09103a68b6eafe6
vigobin <vgobin@outlook.com>
2023-01-23 09:45:50 -0800

    Added file for task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a function that prints a text"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after ., ? and :
+
+    Args:
+        text (str): Must be a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
