885354394ef5089ae732e7b4bcbd92056f905a09
vigobin <vgobin@outlook.com>
2023-01-23 14:11:23 -0600

    Initial commit for task 1

---
+++ 0x06-python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+class Rectangle:
+    """Defines a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """Instantiate a rectangle.
+
+        Args:
+            width (int): Width of rectangle. Defaults to 0.
+            height (int): Height of rectangle. Defaults to 0.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    
+    def area(self):
+        return (self.__width * self.__height)
+    
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return ((self.__width + self.__height) * 2)
