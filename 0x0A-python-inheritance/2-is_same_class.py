6d18075d8b09b954faaf7e5983d0e8dccabf425b
vigobin <vgobin@outlook.com>
2023-01-27 09:59:02 -0600

    Changed from isinstance to type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@
 def is_same_class(obj, a_class):
     """Function that checks if an object is exactly and instance of the class.
     """
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
