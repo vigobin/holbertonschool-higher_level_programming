bf1f8d1118b3ecf5a18fc95fae36c895fdd5cb0e
vigobin <vgobin@outlook.com>
2023-01-27 10:45:54 -0600

    Added file task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines the class BaseGeometry.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
