c9a33fcbc5cbe5cbb88d84e6b6699aa0d00ef37a
vigobin <vgobin@outlook.com>
2023-01-30 11:02:41 -0600

    Added check for file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,5 +5,9 @@
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = load_from_json_file("add_item.json")
+try:
+    args = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    args = []
+
 save_to_json_file(args, "add_item.json")
