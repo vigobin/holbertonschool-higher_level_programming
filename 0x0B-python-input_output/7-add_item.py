91f437264d042b0b9f963e649d7eca5ec885fff5
vigobin <vgobin@outlook.com>
2023-01-30 10:54:40 -0600

    Added json list of arguments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 """Adds arguments to a list"""
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = load_from_json_file("add_item.json")
+save_to_json_file(args, "add_item.json")
