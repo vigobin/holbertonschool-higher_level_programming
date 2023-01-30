527bb9b9f6d130c5537a41732aec87ff34dd4504
vigobin <vgobin@outlook.com>
2023-01-30 13:40:58 -0600

    Initial commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""The Base class"""
+
+
+class Base:
+    """Defines the Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base__nb_objects += 1
+            self.id = Base.__nb_objects
