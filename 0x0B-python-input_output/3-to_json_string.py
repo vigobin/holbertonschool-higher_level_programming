6b3006db146167b24f4eaee540fdb79f4b55692c
vigobin <vgobin@outlook.com>
2023-01-30 10:03:31 -0600

    Changed from print to return

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,4 +6,4 @@ import json
 def to_json_string(my_obj):
     """function that returns the JSON representation of an object (string)
     """
-    print(json.dumps(my_obj))
+    return json.dumps(my_obj)
