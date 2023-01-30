581e2c3fd2e8b7c7442ed74d9186febb6f04279e
vigobin <vgobin@outlook.com>
2023-01-30 10:48:26 -0600

    Initial commit task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Adds arguments to a list"""
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
