6184b9c11dd425f7f75b58812f6eb53d24364ad0
vigobin <vgobin@outlook.com>
2023-01-30 08:35:24 -0600

    Added file for task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""The write_file function"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file (UTF8) and
+    returns the number of characters written.
+    """
+    with open(filename) as f:
+        file1 = f.read()
+        file1.write(text)
