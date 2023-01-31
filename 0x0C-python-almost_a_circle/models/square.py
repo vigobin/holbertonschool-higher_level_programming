610ef03eb9ffcf37f5dfbe8bd3430fd874ec409b
vigobin <vgobin@outlook.com>
2023-01-31 11:47:49 -0600

    Added task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,16 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize the square"""
         super().__init__(size, size, x, y, id)
+
+@property
+def size(self):
+    return self.__width
+
+@size.setter
+def size(self, value):
+    if type(value) != int:
+        raise TypeError("width must be an integer")
+    if value <= 0:
+        raise ValueError("width must be > 0")
+    self.__width = value
+    self.__height = value
