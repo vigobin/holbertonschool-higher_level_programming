3b5ac754c06b893e99b4a77e6355dace38fd7af3
vigobin <vgobin@outlook.com>
2023-01-31 11:53:21 -0600

    Corrected instantiation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        return self.__width
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -19,5 +19,5 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
