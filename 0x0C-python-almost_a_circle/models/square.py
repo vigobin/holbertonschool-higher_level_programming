65d3906c535daf53c0c2eb1953dfd90d1658b705
vigobin <vgobin@outlook.com>
2023-01-31 11:33:32 -0600

    Updated to only include attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,4 +7,4 @@ class Square(Rectangle):
     """Defines the class Square which inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize the square"""
-        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        super().__init__(size, size, x, y, id)
