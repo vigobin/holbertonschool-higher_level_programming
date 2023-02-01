293f9f3e58fc0d09df76daea93d9d9f628dcffed
vigobin <vgobin@outlook.com>
2023-01-31 20:37:34 -0600

    Added to Base for task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,3 +36,13 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set:"""
+        if cls.__name__ == "Rectangle":
+            dummy_attr = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy_attr = cls(1)
+        dummy_attr.update(**dictionary)
+        return dummy_attr
