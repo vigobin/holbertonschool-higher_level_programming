812a7bac5c3414998b5eb67c0ff273ecffd4f46a
vigobin <vgobin@outlook.com>
2023-02-09 09:23:38 -0800

    updated syntax

--- 0x00-SQL_introduction/4-first_table.sql
+++ 0x00-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- Creates a table called first_table
-CREATE TABLE IF NOT EXISTS first_table (
-    first_table_id INT,
-    first_table_name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256);
