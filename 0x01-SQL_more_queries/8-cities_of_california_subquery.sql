f13ad74bf1e8f29335066199f2b4147158b772c7
vigobin <vgobin@outlook.com>
2023-02-13 13:03:29 -0800

    Removed database name

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT id, name FROM hbtn_0d_usa.cities
+SELECT id, name FROM cities
 WHERE state_id IN(
     SELECT id FROM states
     WHERE name = 'California'
