f41554f18d31c0c48f671f845c8bd295bfea6a38
vigobin <vgobin@outlook.com>
2023-02-13 07:04:04 -0800

    Corrected syntax

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- Creates the MySQL server user user_0d_1
-CREATE USER user_0d_1@localhost IDENTIFIED WITH authentication_plugin BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
