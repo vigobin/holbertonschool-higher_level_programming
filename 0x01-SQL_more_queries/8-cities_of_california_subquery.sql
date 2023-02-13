d6414ed85e2f6c7104f222de345f01c934d2cfd8
vigobin <vgobin@outlook.com>
2023-02-13 13:02:21 -0800

    Used IN instead of =

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT id, name FROM hbtn_0d_usa.cities
-WHERE state_id =(
+WHERE state_id IN(
     SELECT id FROM states
     WHERE name = 'California'
 )
