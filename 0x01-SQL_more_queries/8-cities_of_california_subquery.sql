2ed8932b45a4e543f417ed67e5b0c929e60bae68
vigobin <vgobin@outlook.com>
2023-02-13 12:54:41 -0800

    Corrected order by id

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT id, name FROM hbtn_0d_usa.cities
 WHERE state_id = 1
-ORDER BY cities.id ASC;
+ORDER BY id ASC;
