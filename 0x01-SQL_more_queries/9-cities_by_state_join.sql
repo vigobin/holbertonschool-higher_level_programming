8d7bae8a6abd397f476dea10b57f442376c4d8d8
vigobin <vgobin@outlook.com>
2023-02-13 13:26:43 -0800

    Typo on table name

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, state.id
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = state_id
 ORDER BY cities.id ASC;
