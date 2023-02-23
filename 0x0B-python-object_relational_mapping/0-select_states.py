965a5e58743d50cd8f666433ffecf4efab6e003c
vigobin <vgobin@outlook.com>
2023-02-23 12:20:00 -0800

    Initial commit task 0

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+
+def list_states():
+    """Take 3 arguments to list states in ascending order"""
+    db = MySQLdb.connect(host="localhost",
+                        port=3306,
+                        user=argv[1],
+                        passwd=argv[2],
+                        db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    list_states()
