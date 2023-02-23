d7f42bfff9c904560ff07fc0aefb8752e72dec3c
vigobin <vgobin@outlook.com>
2023-02-23 12:21:13 -0800

    Fixed for pycodestyle

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -7,10 +7,10 @@ from sys import argv
 def list_states():
     """Take 3 arguments to list states in ascending order"""
     db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        db=argv[3])
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
