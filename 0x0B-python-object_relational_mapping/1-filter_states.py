4024ba1f9fd748c47728c7dbb4210bfa9d12b242
vigobin <vgobin@outlook.com>
2023-02-23 12:57:22 -0800

    Added binary check

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -13,8 +13,7 @@ def filter_states():
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE UPPER('N%')
-                ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
