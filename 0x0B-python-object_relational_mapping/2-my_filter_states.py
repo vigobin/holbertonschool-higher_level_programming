66095ffae901e6846e2ae6250d247d440010061a
vigobin <vgobin@outlook.com>
2023-02-24 08:06:47 -0800

    Initial commit task 2

---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Filter states by user function"""
+import MySQLdb
+from sys import argv
+
+
+def my_filter_states():
+    """takes in an argument and displays all values in the states table of
+    hbtn_0e_0_usa where name matches the argument."""
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id"
+                .format(argv[4]))
+    query_rows = cur.fetchall()
+
+    for rows in query_rows:
+        print(row)
+    cur.close()
+    db.close()
+
+    if __name__ == "__main__":
+        my_filter_states()
