2aa58039acea132469c350d74866a79f26f1520c
vigobin <vgobin@outlook.com>
2023-02-24 09:56:11 -0800

    corrected for pycodestyle

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -7,10 +7,10 @@ from sys import argv
 def cities_by_state():
     """Lists all cities from the database hbtn_0e_4_usa"""
     db = MySQLdb.connect(host="localhost",
-                            port=3306,
-                            user=argv[1],
-                            passwd=argv[2],
-                            db=argv[3])
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM cities ORDER BY id")
