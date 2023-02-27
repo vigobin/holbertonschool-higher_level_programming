4daae842c4aff338b86c43196f8be62729ae67d1
vigobin <vgobin@outlook.com>
2023-02-27 11:32:38 -0600

    Added task 7

---
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""All states via SQLAlchemy"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+from model_state import Base, State
+
+
+def state_fetch_all():
+    """Lists all State objects from the database hbtn_0e_6_usa"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+
+    if __name__ == "__main__":
+        state_fetch_all()
