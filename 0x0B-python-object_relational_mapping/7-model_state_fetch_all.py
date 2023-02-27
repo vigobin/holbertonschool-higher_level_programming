bb6ba0d5218a21b429ac9b8bfddc5d213f2d1ed7
vigobin <vgobin@outlook.com>
2023-02-27 12:05:22 -0600

    Corrected typo for States

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,7 +15,7 @@ def state_fetch_all():
 
     session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(states.id, states.name))
+        print("{}: {}".format(State.id, State.name))
 
     session.close()
 
