98a9a15270c57fd6588e1b1da1a2e13aab70195b
vigobin <vgobin@outlook.com>
2023-02-27 12:20:49 -0600

    Added task 8

---
+++ 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""First state function"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+from model_state import Base, State
+
+
+def first_state():
+    """Prints the first State object from the database hbtn_0e_6_usa"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    f_state = session.query(State).first()
+    if f_state:
+        print("{}: {}".format(State.id, State.name))
+    else:
+        print("Nothing")
+
+    session.close()
+
+    if __name__ == "__main__":
+        first_state()
