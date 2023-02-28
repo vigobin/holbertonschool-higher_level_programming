623f8ce5d6fd8dd5c9360ad0b62f3c177718aec4
vigobin <vgobin@outlook.com>
2023-02-28 07:59:34 -0800

    updated sql query to use %s

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,24 +9,21 @@ def my_filter_states():
     hbtn_0e_0_usa where name matches the argument
     but is safe from sql injections."""
 
-    if len(argv) == 5:
-        db = MySQLdb.connect(host="localhost",
-                             port=3306,
-                             user=argv[1],
-                             passwd=argv[2],
-                             db=argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE BINARY name='{:s}' ORDER BY id"
-                    .format(argv[4]))
-        query_rows = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id", (argv[4], ))
+    query_rows = cur.fetchall()
 
-        for row in query_rows:
-            print(row)
-        cur.close()
-        db.close()
-    else:
-        return
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
 
-    if __name__ == "__main__":
-        my_filter_states()
+
+if __name__ == "__main__":
+    my_filter_states()
