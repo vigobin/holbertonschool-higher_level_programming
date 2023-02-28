bb8def92a891fdaf50d2e8a91a27f0d65ee84569
vigobin <vgobin@outlook.com>
2023-02-28 09:24:01 -0600

    Added task 13

---
+++ 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Delete states"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+from model_state import Base, State
+
+
+def delete_state():
+    """Cdeletes all State objects with a name containing the letter a from the
+    database hbtn_0e_6_usa"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    state = session.query(State).all()
+    for row in state:
+        if 'a' in row.name:
+            session.delete()
+
+    session.close()
+
+    if __name__ == "__main__":
+        delete_state()
