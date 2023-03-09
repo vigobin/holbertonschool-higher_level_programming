e06ba6ac5718ba20337d77227cb9df404caaa91a
vigobin <vgobin@outlook.com>
2023-03-09 12:44:47 -0600

    added prints

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -3,8 +3,15 @@
 import urllib.request
 
 
-def fetch:
-"""Python script that fetches https://intranet.hbtn.io/status"""
-    with urllib.request.urlopen(https://intranet.hbtn.io/status) as response:
+def fetch():
+    """Python script that fetches https://intranet.hbtn.io/status"""
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
-        print(html)
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    fetch()
