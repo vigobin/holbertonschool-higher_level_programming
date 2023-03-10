3cf1c2d06b10f518f1cdaf67d6b68b82b7a0df17
vigobin <vgobin@outlook.com>
2023-03-10 13:08:19 -0600

    Initial commit task 5

---
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Response header value"""
+from sys import argv
+import requests
+
+
+def response():
+    """takes in a URL, sends a request to the URL and displays the
+    value of the variable X-Request-Id"""
+    response = requests.get(argv[1])
+    r = response.headers
+    print(r['X-Request-Id'])
+
+
+if __name__ == '__main__':
+    response()
