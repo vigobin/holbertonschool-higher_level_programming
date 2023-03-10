20fcb4785bf267ac85ef1d7b0935956d4a064613
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 <vgobin@outlook.com>
2023-03-10 17:34:45 +0000

    Updated err

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -9,8 +9,8 @@ def error():
         with request.urlopen(argv[1]) as response:
             r = response.read()
             print(r.decode('utf-8'))
-    except error.HTTPError as error:
-        print("Error code: {}".format(error.code))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
 
 
 if __name__ == "__main__":
