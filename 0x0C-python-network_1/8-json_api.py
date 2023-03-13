15dea23d9d39e8c22e3f3c7efa37295a0034bd05
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 <vgobin@outlook.com>
2023-03-13 16:41:03 +0000

    Task 8

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Search API"""
+import requests
+from sys import argv
+
+
+def search():
+    """akes in a letter and sends a POST request to the url
+    with the letter as a parameter."""
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    
+    values = {'q': q}
+
+    r = requests.post(url, data=values)
+    try:
+        data = r.json()
+        if len(data) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(data['id'], data['name']))
+    except ValueError as e:
+        print('Not a valid JSON')
+
+
+if __name__ == "__main__":
+    search()
