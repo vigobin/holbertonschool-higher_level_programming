142bbfd9eedfbee8a6adf63877583f978c11338a
vigobin <vgobin@outlook.com>
2023-04-06 14:53:38 -0500

    semistandard ===

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -2,8 +2,8 @@
 
 if (process.argv.length < 3) {
   console.log('No argument');
-} else if (process.argv.length == 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('Arguments found')
+  console.log('Arguments found');
 }
