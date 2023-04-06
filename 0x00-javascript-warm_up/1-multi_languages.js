0bfb0e788c6f9a5b1f495d897a8e9198785a1146
vigobin <vgobin@outlook.com>
2023-04-06 14:23:13 -0500

    semistandard space

--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,4 @@
 const myVar1 = 'C is fun';
 const myVar2 = 'Python is cool';
 const myVar3 = 'JavaScript is amazing';
-console.log(myVar1 + '\n' + myVar2 + '\n'+ myVar3);
+console.log(myVar1 + '\n' + myVar2 + '\n' + myVar3);
