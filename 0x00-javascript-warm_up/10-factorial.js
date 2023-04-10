83121e8aadfd3b01c910b0e4bb89d8118b4d1e38
vigobin <vgobin@outlook.com>
2023-04-10 15:12:31 -0500

    NaN check

--- 0x00-javascript-warm_up/10-factorial.js
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 
 function factorial (n) {
-  if (n === 0 || n < 0) {
+  if (n < 1) {
     return 1;
   }
   return n * factorial(n - 1);
 }
 
 const x = parseInt(process.argv[2]);
-console.log(factorial(x));
+if (isNaN(x)) {
+  console.log(1);
+} else {
+  console.log(factorial(x));
+}
