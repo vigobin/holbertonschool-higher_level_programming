f9b9dc40ecd14e9bce847217400b8843560a97fd
vigobin <vgobin@outlook.com>
2023-04-10 16:14:24 -0500

    semistandard

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -5,9 +5,8 @@ if (process.argv.length < 3) {
 } else if (process.argv.length === 3) {
   console.log(0);
 } else {
-  let args = process.argv.slice(2);
-  console.log(args)
+  const args = process.argv.slice(2);
   args.sort();
   args.reverse();
-  console.log(args[1])
+  console.log(args[1]);
 }
