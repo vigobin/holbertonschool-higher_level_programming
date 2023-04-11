2fec4cc84dcb30525e53d72933c17fc461ce6783
vigobin <vgobin@outlook.com>
2023-04-11 11:46:41 -0500

    Task 2

---
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+}
+module.exports = Rectangle;
