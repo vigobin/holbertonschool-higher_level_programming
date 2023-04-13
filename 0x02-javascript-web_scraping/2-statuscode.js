5935684005999846787de17ec0ed5b15f5469245
vigobin <vgobin@outlook.com>
2023-04-13 18:13:57 +0000

    Updated print

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 request
   .get(process.argv[2])
   .on('response', function (response) {
-    console.log(response.statusCode);
+    console.log('code: ' + response.statusCode);
   });
